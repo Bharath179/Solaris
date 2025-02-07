0789afd289e25357452629eefbd8121c2635de7a
bharathkn179@gmail.com <bharathkn179@gmail.com>
2025-02-07 11:57:38 +0530

    modified

--- test_cases/ticketing.py
+++ test_cases/ticketing.py
@@ -37,27 +37,25 @@ class Test_Ticketing:
             ScreenshotUtil.take_screenshot(self.driver, "test_visibility_of_navigation_bar")
             raise RuntimeError("Navigation bar is not visible. Test failed.")
 
-        self.ticket.click_ticketing_bar()
-        time.sleep(2)
-        self.ticket.click_ticket_button()
-        time.sleep(2)
-        self.ticket.click_category_dropdown()
-        time.sleep(2)
-        self.ticket.click_select_category_issue()
-        time.sleep(2)
-        self.ticket.click_priority_dropdown()
-        time.sleep(2)
-        self.ticket.select_option_from_priority_dropdown()
-        time.sleep(2)
-        self.ticket.click_user_dropdown()
-        time.sleep(2)
-        self.ticket.select_option_from_user_dropdown()
-        time.sleep(2)
-        self.ticket.click_on_issue_time()
-        time.sleep(2)
-        self.ticket.select_date_from_calender()
-        time.sleep(2)
-        self.ticket.select_textarea_field()
+        actions = [
+            self.ticket.click_ticketing_bar,
+            self.ticket.click_ticket_button,
+            self.ticket.click_category_dropdown,
+            self.ticket.click_select_category_issue,
+            self.ticket.click_priority_dropdown,
+            self.ticket.select_option_from_priority_dropdown,
+            self.ticket.click_user_dropdown,
+            self.ticket.select_option_from_user_dropdown,
+            self.ticket.click_on_issue_time,
+            self.ticket.select_date_from_calender,
+            self.ticket.select_textarea_field
+        ]
+
+        # Loop through the actions and execute each one with a short delay
+        for action in actions:
+            action()
+            time.sleep(2)
+
         time.sleep(5)
         file_upload=self.ticket.click_on_attachment_field()
         if file_upload.is_displayed():
